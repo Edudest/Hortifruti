dc74f524a34acd1fbc94efe6f966673584bc58af
Eduardo Yoshida Reis <eduardoyreis2006@gmail.com>
2025-06-05 11:54:13 -0300

    primeiro commit

---
+++ src/main/java/br/edu/fiec/Hortifruti/HortifrutiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fiec.Hortifruti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortifrutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortifrutiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fiec/Hortifruti/HortifrutiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fiec.Hortifruti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortifrutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
